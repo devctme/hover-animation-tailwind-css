7f6be18f6c158399b9b2b317eda60dea267a0d16
Md Minarul islam <140134286+devctme@users.noreply.github.com>
2024-01-18 10:05:32 +0600

    Update howver.tsx

--- howver.tsx
+++ howver.tsx
@@ -1,1 +1,13 @@
 
+ <ul className=" md:inline-flex gap-7 items-center text-gray-900 hover:text-black duration-200">
+          {navigation.map((item, idx) => (
+            <Link
+              key={idx}
+              href={item.href}
+              className="text-sm uppercase font-semibold relative group overflow-hidden"
+            >
+              {item.title}
+              <span className="w-full h-[2px] bg-blue-700 absolute inline-block left-0 bottom-0 -translate-x-[100%] group-hover:translate-x-0 transition-transform duration-200" />
+            </Link>
+          ))}
+        </ul>
